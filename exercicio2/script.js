16a43f8efdade3d322def16be930baccc888bfbe
Heytordesouza <heytordesnascimento@gmail.com>
2022-09-06 13:31:24 -0300

    Exercícios 1,2 e 3 a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,43 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+/*# Exercício 2
 
-if(){
+Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa 
+terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando 
+alguma faculdade.
 
+Crie um `if` para cada variável, checando as seguintes afirmacões:
+- Se a pessoa tem 18 anos ou mais;
+- Se a pessoa terminou o ensino médio;
+- Se a pessoa NÃO está cursando alguma faculdade;
+
+Crie um `if` que imprima que a afirmacão é verdadeira, 
+e um `else` para imprimir que a afirmacão não é verdadeira.
+
+Caso a primeira pessoa tenha idade >=18:
+
+<b>"A pessoa é maior de idade"</b>
+
+e caso não seja:
+
+<b>"A pessoa é menor de idade"</b>*/
+
+let idade = Number(prompt("Você tem quantos anos?"))
+let isTerminouEnsinoMedio = confirm("Você terminou o ensino médio?")
+let isCursandoOutraFaculdade = confirm("Você não está cursando outra faculdade?")
+
+if(idade>=18){
+    console.log("A pessoa é maior de idade")
 }else{
-	
+    console.log("A pessoa é menor de idade")
+}
+
+if(isTerminouEnsinoMedio===true){
+    console.log("A pessoa terminou o ensino médio")
+}else {
+    console.log("A pessoa não terminou o ensino médio")
+}
+
+if(isCursandoOutraFaculdade===true){
+    console.log("A pessoa não está cursando outra faculdade")
+}else {
+    console.log("A pessoa está cursando outra faculdade")
 }
